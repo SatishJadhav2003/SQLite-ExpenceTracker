f34dd9e9ec0d92852cdc69321ce97f68099d2763
Satish Jadhav <sj8390613529@gmail.com>
2024-02-11 19:11:42 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'SQLite-ExpenceTracker',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
